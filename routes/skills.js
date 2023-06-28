d82019089d2667e12e16e2dfde90066fb65899c4
vaidehi <vaidehi117v@gmail.com>
2023-06-27 22:24:50 -0400

    server

--- routes/skills.js
+++ routes/skills.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var skillsCtrl = require('../controllers/skills');
-//All acuatal paths starts with "/skills"
 
+//All acuatal paths starts with "/skills"
+//get request '/skills'
+router.get('/', skillsCtrl.index);
 
 module.exports = router;
