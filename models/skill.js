1c793ef0dbfbfa0a279df3e1200342baac9dcf8d
vaidehi <vaidehi117v@gmail.com>
2023-06-27 21:56:01 -0400

    modules controllers

---
+++ models/skill.js
@@ -0,0 +1,15 @@
+const todos = [
+    {id: 125223, skill: 'Java Script', done: true},
+    {id: 127904, skill: 'CSS', done: true},
+    {id: 139608, skill: 'HTML', done: true},
+    {id: 137502, skill: 'Python', done: false}
+  ];
+  
+  module.exports = {
+    getAll
+  };
+  
+  function getAll() {
+    return skills;
+  }
+ 
---
+++ routes/skills.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+var skillsCtrl = require('../controllers/skills');
+//All acuatal paths starts with "/skills"
+
+
+module.exports = router;
